6c9c6a3cfb3f84d1ca65b5e5d4e54aecfb3c0be2
Toto97420 <65169657+Toto97420@users.noreply.github.com>
2020-11-20 15:59:50 +0400

    Ajout des classes ajout_client.php/footer.php/header.php/index.php/ dans dossier client et modis header.php du dossier pour ajouter la rubrique Connexion

---
+++ client/header.php
@@ -0,0 +1,34 @@
+<head>
+    <title>Accueil</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo $ROOTCSS_JS ?>css/admin/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="sidebar_btn">
+            <span><i class="fa fa-bars"></i></span>
+        </div>
+        <div class="left_area">
+        <a href="<?php echo $ROOT?>../"><h3>Le Bon Barquette</h3></a>
+        </div>
+        <div class="left_area">
+        <a href="<?php echo $ROOT?>./"><h3>Formulaire d'inscription / Connexion</h3></a>
+        </div>
+    </header>
+    <div class="sidebar toggled">
+        <h4>Menu</h4>
+        <a href="<?php echo $ROOT?>../"><i class="fa fa-home"></i><span>Accueil</span></a>
+        <!--<a href="<?php echo $ROOT?>plat/"><i class="fa fa-drumstick-bite"></i><span>Repas</span></a>-->
+        <a href="<?php echo $ROOT?>../"><i class="fa fa-phone"></i><span>Contact</span></a>
+        <a href="<?php echo $ROOT?>../"><i class="fa fa-user-shield"></i><span>Administration</span></a>
+    </div>
+    <div class="container content">
--- header.php
+++ header.php
@@ -37,6 +37,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo $ROOT?>admin/">Administration</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo $ROOT?>client/">Inscription/Connection</a>
+                    </li>
                 </ul>
             </div>
         </div>
