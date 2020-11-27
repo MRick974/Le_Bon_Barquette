6b4d51712d786dc8078a811faf2739e7822c39ab
Mrick <p.hoareau@rt-iut.re>
2020-11-27 15:04:41 +0400

    ajout lien commander plat

--- index.php
+++ index.php
@@ -58,6 +58,7 @@ $repas=$requete->fetchAll();
                                     </a>
                                     <p class="price"><?php echo $plats->getPrix()."€";?></p>
                                     <div style="padding-left:30px;position:static;">
+                                    <a href="client/panier/fonctionpanier.php?id=<?php echo $plats->getId(); ?>" class="barq-link">
                                         <button class="btn-barq-primary">Commander<i class="fas fa-plus" style="margin-left:10px;"></i></button>
                                     </div>
                                 </div>
